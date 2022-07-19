34af71c28a112fdbc176db4b1604f854d8fd4f25
Hanbyeol <davidlee9088@gmail.com>
2022-07-18 17:59:20 -0700

    second update

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ let seattle = {
     }
   },
   render: function () {
+
     this.calCookiesPerHour();
     let dataSheetContainer = document.getElementById('test1');
     let ul = document.createElement('ul');
@@ -36,14 +37,233 @@ let seattle = {
 
     let totalSign = document.createElement('li');
     ul.appendChild(totalSign)
-    totalSign.textContent = Total;
-
-
+    function sumArray(cookieNumberArray) {
+      let sum = 0
+      for (let i = 0; i < cookieNumberArray.length; i += 1) {
+        sum += cookieNumberArray[i]
+      }
+      console.log(`sum ${sum}`);
+      return sum
+    }
+    let total = sumArray(this.cookieNumberArray);
+    totalSign.textContent = `Total: ${total} Cookies`
   }
+
 }
 
 seattle.render();
 console.log(seattle);
+
+
+let tokyo = {
+  name: 'Tokyo',
+  cookieNumberArray: [],
+  min: 3,
+  max: 24,
+  avg: 1.2,
+  getRandomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  calCookiesPerHour: function () {
+    for (let i = 0; i < hour.length; i++) {
+      let customersThisHour = this.getRandomCustomers();
+      let cookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
+      this.cookieNumberArray.push(cookiesSoldThisHour)
+      console.log(cookiesSoldThisHour);
+    }
+  },
+  render: function () {
+
+    this.calCookiesPerHour();
+    let dataSheetContainer = document.getElementById('test1');
+    let ul = document.createElement('ul');
+    dataSheetContainer.appendChild(ul)
+    ul.setAttribute('class', 'tokyoCookies')
+    ul.textContent = 'Tokyo Hourly List of cookies'
+
+    for (let i = 0; i < hour.length; i++) {
+      let li = document.createElement('li');
+
+      li.textContent = `${hour[i]}: ${this.cookieNumberArray[i]} Cookies`
+      ul.appendChild(li)
+    }
+
+    let totalSign = document.createElement('li');
+    ul.appendChild(totalSign)
+    function sumArray(cookieNumberArray) {
+      let sum = 0
+      for (let i = 0; i < cookieNumberArray.length; i += 1) {
+        sum += cookieNumberArray[i]
+      }
+      console.log(`sum ${sum}`);
+      return sum
+    }
+    let total = sumArray(this.cookieNumberArray);
+    totalSign.textContent = `Total: ${total} Cookies`
+  }
+
+}
+
+tokyo.render();
+console.log(tokyo);
+
+let dubai = {
+  name: 'Dubai',
+  cookieNumberArray: [],
+  min: 11,
+  max: 38,
+  avg: 3.7,
+  getRandomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  calCookiesPerHour: function () {
+    for (let i = 0; i < hour.length; i++) {
+      let customersThisHour = this.getRandomCustomers();
+      let cookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
+      this.cookieNumberArray.push(cookiesSoldThisHour)
+      console.log(cookiesSoldThisHour);
+    }
+  },
+  render: function () {
+
+    this.calCookiesPerHour();
+    let dataSheetContainer = document.getElementById('test1');
+    let ul = document.createElement('ul');
+    dataSheetContainer.appendChild(ul)
+    ul.setAttribute('class', 'dubaiCookies')
+    ul.textContent = 'Dubai Hourly List of cookies'
+
+    for (let i = 0; i < hour.length; i++) {
+      let li = document.createElement('li');
+
+      li.textContent = `${hour[i]}: ${this.cookieNumberArray[i]} Cookies`
+      ul.appendChild(li)
+    }
+
+    let totalSign = document.createElement('li');
+    ul.appendChild(totalSign)
+    function sumArray(cookieNumberArray) {
+      let sum = 0
+      for (let i = 0; i < cookieNumberArray.length; i += 1) {
+        sum += cookieNumberArray[i]
+      }
+      console.log(`sum ${sum}`);
+      return sum
+    }
+    let total = sumArray(this.cookieNumberArray);
+    totalSign.textContent = `Total: ${total} Cookies`
+  }
+
+}
+
+dubai.render();
+console.log(dubai);
+
+
+let paris = {
+  name: 'paris',
+  cookieNumberArray: [],
+  min: 20,
+  max: 38,
+  avg: 2.3,
+  getRandomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  calCookiesPerHour: function () {
+    for (let i = 0; i < hour.length; i++) {
+      let customersThisHour = this.getRandomCustomers();
+      let cookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
+      this.cookieNumberArray.push(cookiesSoldThisHour)
+      console.log(cookiesSoldThisHour);
+    }
+  },
+  render: function () {
+
+    this.calCookiesPerHour();
+    let dataSheetContainer = document.getElementById('test1');
+    let ul = document.createElement('ul');
+    dataSheetContainer.appendChild(ul)
+    ul.setAttribute('class', 'parisCookies')
+    ul.textContent = 'Paris Hourly List of cookies'
+
+    for (let i = 0; i < hour.length; i++) {
+      let li = document.createElement('li');
+
+      li.textContent = `${hour[i]}: ${this.cookieNumberArray[i]} Cookies`
+      ul.appendChild(li)
+    }
+
+    let totalSign = document.createElement('li');
+    ul.appendChild(totalSign)
+    function sumArray(cookieNumberArray) {
+      let sum = 0
+      for (let i = 0; i < cookieNumberArray.length; i += 1) {
+        sum += cookieNumberArray[i]
+      }
+      console.log(`sum ${sum}`);
+      return sum
+    }
+    let total = sumArray(this.cookieNumberArray);
+    totalSign.textContent = `Total: ${total} Cookies`
+  }
+
+}
+
+paris.render();
+console.log(paris);
+
+let lima = {
+  name: 'Seattle',
+  cookieNumberArray: [],
+  min: 2,
+  max: 16,
+  avg: 4.6,
+  getRandomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  calCookiesPerHour: function () {
+    for (let i = 0; i < hour.length; i++) {
+      let customersThisHour = this.getRandomCustomers();
+      let cookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
+      this.cookieNumberArray.push(cookiesSoldThisHour)
+      console.log(cookiesSoldThisHour);
+    }
+  },
+  render: function () {
+
+    this.calCookiesPerHour();
+    let dataSheetContainer = document.getElementById('test1');
+    let ul = document.createElement('ul');
+    dataSheetContainer.appendChild(ul)
+    ul.setAttribute('class', 'limaCookies')
+    ul.textContent = 'Lima Hourly List of cookies'
+
+    for (let i = 0; i < hour.length; i++) {
+      let li = document.createElement('li');
+
+      li.textContent = `${hour[i]}: ${this.cookieNumberArray[i]} Cookies`
+      ul.appendChild(li)
+    }
+
+    let totalSign = document.createElement('li');
+    ul.appendChild(totalSign)
+    function sumArray(cookieNumberArray) {
+      let sum = 0
+      for (let i = 0; i < cookieNumberArray.length; i += 1) {
+        sum += cookieNumberArray[i]
+      }
+      console.log(`sum ${sum}`);
+      return sum
+    }
+    let total = sumArray(this.cookieNumberArray);
+    totalSign.textContent = `Total: ${total} Cookies`
+  }
+
+}
+
+lima.render();
+console.log(lima);
+
 
 //grab the element in Dom 
 // let dataSheetContainer = document.getElementById('test1');
